8984ba67118e9f9db2dd5e501b4f6fe4e5a2d83e
Gregory Haney <gregory.haney@gmail.com>
2022-01-27 08:55:07 -0600

    ADD: update code with addEventListener prebuilt code

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,5 +1,29 @@
 import { getWalkers } from "./database.js"
 
+/*
+prebuilt code added for EventListener:
+*/
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("walker")) {
+            const [,walkerId] = itemClicked.id.split("--")
+
+            for (const walker of walkers) {
+                if (walker.id === parseInt(walkerId)) {
+                    window.alert(`${walker.name} services ${walker.city}`)
+                }
+            }
+        }
+    }
+)
+
+
+/*
+end of prebuilt code insertion for EventListener
+*/
+
 const walkers = getWalkers()
 
 
@@ -7,10 +31,7 @@ export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
-        walkerHTML += `
-                    <li>
-                    ${walker.name}
-                    </li>`
+        walkerHTML += `<li id="walker--${walker.id}">${walker.name}</li>`
     }
 
     walkerHTML += "</ul>"
