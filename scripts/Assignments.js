3425f1e6e5eb927ae863ba1325e11fec13afbf4c
Gregory Haney <gregory.haney@gmail.com>
2022-01-31 09:47:46 -0600

    DEBUGGING: Shows current assignment. Incorrect o/p for clicked walker

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -11,48 +11,42 @@ const cities = getCities()
 const walkerCities = getWalkerCities()
     // properties of 'walkerCities' will be: id, cityId, walkerId
 
-export const Assignments = () => { 
-    
-    const findWalkerAssignedToPet = (pets, walkers, walkerCities, cities) => {
-        let walkerAssignedToPetArray = []   
-        let assignmentHTML = ""
-        assignmentHTML = "<ul>"     
-                     
-             for (const pet of pets) {
-                 for (const walker of walkers) {
-                     if (walker.id === pet.walkerId) {
-                         for (const walkerCity of walkerCities) {  
-                             if (walker.id === walkerCity.walkerId) {
-                                 for (const city of cities) {               
-                                     if (city.id === walkerCity.cityId) {
-                                                 const walkerAssignedToPetObject = {
-                                                 walkerId: walker.id,
-                                                 walkerName: walker.name,
-                                                 petId: pet.id,
-                                                 petName: pet.name,
-                                                 petWalkerId: pet.walkerId,
-                                                 cityName: city.name
-                                             }
-                                     walkerAssignedToPetArray.push(walkerAssignedToPetObject)
-                                                     assignmentHTML += `
-                                                     <li>
-                                                         ${walkerAssignedToPetObject.petName} is being walked by
-                                                         ${walkerAssignedToPetObject.walkerName} in 
-                                                         ${walkerAssignedToPetObject.cityName}
-                                                     </li>
-                                                     `
-                                     }                              
-                                 }
-                             }
-                         } 
-                     }                   
-                 }   
-             }    
-             // end the HTML string by closing out the unordered list
-             // return the HTML string for use in the index.html page
-             assignmentHTML += "</ul>"
 
- return assignmentHTMLs
- }
- 
+export const Assignments = () => { 
+    const assignmentHTML = findWalkerAssignedToPet(pets, walkers, walkerCities, cities)
+    return assignmentHTML
 }
+
+
+const findWalkerAssignedToPet = (pets, walkers, walkerCities, cities) => {
+    let walkerAssignedToPetArray = []   
+    let assignmentHTML = ""
+    assignmentHTML = "<ul>"     
+                    
+            for (const pet of pets) {
+                for (const walker of walkers) {
+                    if (walker.id === pet.walkerId) {
+                        for (const walkerCity of walkerCities) {  
+                            if (walker.id === walkerCity.walkerId) {
+                                for (const city of cities) {               
+                                    if (city.id === walkerCity.cityId) {
+                                    assignmentHTML += `
+                                                    <li>
+                                                        ${pet.name} is being walked by
+                                                        ${walker.name} in 
+                                                        ${city.name}
+                                                    </li>
+                                                    `
+                                    }                              
+                                }
+                            }
+                        } 
+                    }                   
+                }   
+            }    
+            // end the HTML string by closing out the unordered list
+            // return the HTML string for use in the index.html page
+    assignmentHTML += "</ul>"
+            
+return assignmentHTML
+} 
